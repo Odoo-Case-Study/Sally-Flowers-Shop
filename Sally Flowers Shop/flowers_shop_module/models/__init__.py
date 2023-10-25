c022b567ee77c7a6429a74cca6d41f8bd54a1257
GhazalGithub <145249867+GhazalGithub@users.noreply.github.com>
2023-10-25 20:22:59 +0300

    Delete Sally Flowers Shop/flowers_shop_module directory
    delete

--- Sally Flowers Shop/flowers_shop_module/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import flower
-from . import product
-from . import stock_production_lot
-from . import flower_water
-from . import sale_order_inherit
-from . import stock_warehouse
-from . import stock_warehouse_weather
